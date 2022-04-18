5a160aa59381048f446839419e8a0cd742baa809
yibzhang <zhangyiboturku@gmail.com>
2022-04-18 21:35:33 +0300

    add testing data

--- cosmeticapp/src/App.js
+++ cosmeticapp/src/App.js
@@ -1,4 +1,18 @@
+var cosmetics = require('./testdata/cosmetics.json');
+var ingredients = require('./testdata/ingredients.json');
+
+const Cosmetic = (props) => {
+  return (
+    <div>
+      <img src={props.picture}/>
+    </div>
+  )
+}
+
 function App() {
+  console.log(cosmetics);
+  console.log(ingredients);
+
   return (
     <div>
       <p>hello cosmetics</p>
