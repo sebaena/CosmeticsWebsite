2193dc60ac5944471c3dc3120790d8dd6bdfb2e3
yibzhang <zhangyiboturku@gmail.com>
2022-04-19 12:15:35 +0300

    add component Cosmetic to display the testdata

--- cosmeticapp/src/App.js
+++ cosmeticapp/src/App.js
@@ -1,21 +1,34 @@
-var cosmetics = require('./testdata/cosmetics.json');
-var ingredients = require('./testdata/ingredients.json');
+import { useState } from "react";
+
+var cosmeticsList = require("./testdata/cosmetics.json");
+var ingredientsList = require("./testdata/ingredients.json");
 
 const Cosmetic = (props) => {
+  const { cosmetic } = props;
+
   return (
     <div>
-      <img src={props.picture}/>
+      <img src={cosmetic.picture} />
+      <p>Cosmetic Name : {cosmetic.name}</p>
+      <p>ingredients:</p>
+      <ul>
+        {cosmetic.ingredients.map((ingredient) => (
+          <li key={ingredient.name}>{ingredient.name}</li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 function App() {
-  console.log(cosmetics);
-  console.log(ingredients);
+  const [cosmetic, setCosmetic] = useState(cosmeticsList[0]);
+  console.log(cosmeticsList[0]);
+  console.log(ingredientsList);
 
   return (
     <div>
       <p>hello cosmetics</p>
+      <Cosmetic cosmetic={cosmetic} />
     </div>
   );
 }
