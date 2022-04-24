b57a047586309724eef90cca5f7c899d0db3ef15
Sebastian Villegas <sebaena@gmail.com>
2022-04-24 12:14:45 +0300

    catched errors

--- cosmeticapp/src/services/cosmetic.js
+++ cosmeticapp/src/services/cosmetic.js
@@ -3,22 +3,22 @@ const baseUrl = process.env.REACT_APP_URL + "cosmeticsList";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
-  return request.then((response) => response.data);
+  return request.then((response) => response.data).catch(error => console.log("get all error"));
 };
 
 const getOne = (id) => {
   const request = axios.get(`${baseUrl}/${id}`);
-  return request.then((response) => response.data);
+  return request.then((response) => response.data).catch(error => console.log("get one  error"));
 };
 
 const create = (newObject) => {
   const request = axios.post(baseUrl, newObject);
-  return request.then((response) => response.data);
+  return request.then((response) => response.data).catch(error => console.log("create  error"));
 };
 
 const update = (id, newObject) => {
   const request = axios.put(`${baseUrl}/${id}`, newObject);
-  return request.then((response) => response.data);
+  return request.then((response) => response.data).catch(error => console.log("update error"));
 };
 
 export default {
