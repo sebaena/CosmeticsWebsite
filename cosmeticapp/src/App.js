b3e102488ffb0118d721abeaea45def175623770
Sebastian Villegas <sebaena@gmail.com>
2022-07-02 14:53:04 +0300

    played with css

--- cosmeticapp/src/App.js
+++ cosmeticapp/src/App.js
@@ -9,24 +9,24 @@ import {
 
 function App() {
 
-  const padding = {
-    padding: 5
-  }
+  // const padding = {
+  //   padding: 5
+  // }
 
   return (
     <Router>
-      <div>
-        <Link style={padding} to = "/"> Home </Link>
-        <Link style={padding} to = "/products"> Products </Link>
-        <Link style={padding} to = "/signin"> SignIn </Link>
-      </div>
+      <nav className="navbar">
+        <div className="navbar-menu">
+          <Link className="navbar-link" to = "/"> Home </Link>
+          <Link className="navbar-link" to = "/products"> Products </Link>
+          <Link className="navbar-link" to = "/signin"> SignIn </Link>
+        </div>
+      </nav>
       <Routes>
         <Route path="/" element = {<Home />} />
         <Route path="/products" element = {<Products />} />
         <Route path="/signin" element = {<Login />} />
       </Routes>
-
-
     </Router>
   )
 
--- cosmeticapp/src/index.js
+++ cosmeticapp/src/index.js
@@ -3,26 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import cosmeticReducer from "./reducers/cosmeticReducer";
-import ingredientReducer from "./reducers/ingredientReducer";
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
 import store from "./store.js"
 
-// import axios from 'axios'
-
-// const consmeticsListPromise = axios.get('http://localhost:3001/cosmeticsList')
-// console.log(consmeticsListPromise)
-
-// const ingredientsListPromise = axios.get('http://localhost:3001/ingredientsList')
-// console.log(ingredientsListPromise)
-
-// const store = configureStore({
-//   reducer: {
-//     cosmetic: cosmeticReducer,
-//     ingredient: ingredientReducer,
-//   }
-// });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -30,14 +13,3 @@ root.render(
     <App />
   </Provider>
 );
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals();
