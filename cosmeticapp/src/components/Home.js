0501b5410e3581d484e3489c016db6c03aac18b3
Sebastian Villegas <sebaena@gmail.com>
2022-07-10 00:58:56 +0300

    added homescreen animation

--- cosmeticapp/src/components/Home.js
+++ cosmeticapp/src/components/Home.js
@@ -2,8 +2,10 @@
 
 const Home = () => {
     return(
-        <div>
-            <p> Home Screen</p>
+
+        <div className="home-container"> 
+            <span className="text1">Welcome to</span>
+            <span className="text2">The Cosmetics Application</span>
         </div>
     )
 };
