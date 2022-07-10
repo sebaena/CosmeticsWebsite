db94b1d9659a35cdb9f3e4f295aedfba456c12e1
yibzhang <zhangyiboturku@gmail.com>
2022-07-10 21:51:41 +0300

    fix the ingredient display issue for different cosmetic

--- cosmeticapp/src/components/Cosmetic.js
+++ cosmeticapp/src/components/Cosmetic.js
@@ -1,42 +1,82 @@
-import Ingredient from "./IngredientsList";
-import CosmeticDisplay from "./CosmeticDisplay";
+//import Ingredient from "./IngredientsList";
+//import CosmeticDisplay from "./CosmeticDisplay";
+//import SelectedIngredient from "./SelectedIngredient";
+//import { useDispatch } from "react-redux";
+//import { useSelector } from "react-redux";
+//import ingredient from "../services/ingredient";
+//import { useState } from "react";
 
+import { useState } from "react";
+import ingredientService from "../services/ingredient";
 
+const Cosmetic = (props) => {
+  const [selectedIngredient, setSelectedIngredient] = useState("");
+  const [ingredientFeature, setIngredientFeature] = useState("");
 
-import SelectedIngredient from "./SelectedIngredient";
-import { useDispatch } from "react-redux";
-import { useSelector } from 'react-redux';
+  const selectedIngredientHandle = (ingredientName) => {
+    setSelectedIngredient(ingredientName);
+    // get the ingredient function from the database
+    ingredientService.getByQuery(ingredientName).then((returnedIngredient) => {
+      setIngredientFeature(
+        returnedIngredient.length
+          ? returnedIngredient[0].function
+          : "No information"
+      );
+    });
+  };
 
-
-
-
-const Cosmetic = () => {
-  const dispatch = useDispatch();
-  const currentCosmetic = useSelector((state)=> state.cosmetic.currentCosmetic);
-  return Object.keys(currentCosmetic).length > 0 ?(
-    <div className="cosmetic-wrapper">
-        { currentCosmetic.map ((cosmetic, index) =>(
-          <div className="cosmetic" key={index}>
-            <CosmeticDisplay current_cosmetic={cosmetic} />
-            <Ingredient ingredients={cosmetic.ingredients} />
-          </div>
-
-        ))}
-      {/* <div className="cosmetic">
-        <CosmeticDisplay />
-        <Ingredient />
-      </div>
-      <div className="cosmetic">
-        <CosmeticDisplay />
-        <Ingredient />
-      </div>
-      <div className="cosmetic">
-        <CosmeticDisplay />
-        <Ingredient />
-      </div> */}
+  return (
+    <div className="cosmetic">
+      {/* cosmetic image */}
+      <img src={props.cosmetic.picture} />
+      {/* cosmetic title */}
+      <p className="cosmetic-title">{props.cosmetic.name}</p>
+      {/* cosmetic ingredients list */}
+      {props.cosmetic.ingredients.map((ingredient, index) => (
+        <div key={index}>
+          <button
+            className="button-ingredient"
+            key={ingredient.name}
+            onClick={() => selectedIngredientHandle(ingredient.name)}
+          >
+            {ingredient.name}
+          </button>
+          {/* if seleteted ingredient name is the same as current one then display the feature */}
+          {selectedIngredient == ingredient.name ? (
+            <div>{ingredientFeature}</div>
+          ) : (
+            <></>
+          )}
+        </div>
+      ))}
     </div>
-  ) :( <></>)
+  );
 
+  // const dispatch = useDispatch();
+  // const currentCosmetic = useSelector((state)=> state.cosmetic.currentCosmetic);
+  // return Object.keys(currentCosmetic).length > 0 ?(
+  //   <div className="cosmetic-wrapper">
+  //       { currentCosmetic.map ((cosmetic, index) =>(
+  //         <div className="cosmetic" key={index}>
+  //           <CosmeticDisplay current_cosmetic={cosmetic} />
+  //           <Ingredient ingredients={cosmetic.ingredients} />
+  //         </div>
+
+  //       ))}
+  //     {/* <div className="cosmetic">
+  //       <CosmeticDisplay />
+  //       <Ingredient />
+  //     </div>
+  //     <div className="cosmetic">
+  //       <CosmeticDisplay />
+  //       <Ingredient />
+  //     </div>
+  //     <div className="cosmetic">
+  //       <CosmeticDisplay />
+  //       <Ingredient />
+  //     </div> */}
+  //   </div>
+  // ) :( <></>)
 };
 
 export default Cosmetic;
--- cosmeticapp/src/components/Products.js
+++ cosmeticapp/src/components/Products.js
@@ -1,44 +1,43 @@
-
 import { useState, useEffect } from "react";
-import Cosmetic from "./Cosmetic";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import Cosmetic from "./Cosmetic";
+
 // import {clearSelectedIngredient} from "../reducers/ingredientReducer"
 
 import {
-    initializeCosmetics,
-    updateAllCosmeticIds,
-    updateCurrentIndex,
-    updateCurrentCosmetic,
-    nextCosmetic,
-    findCosmetic,
-    clearSelectedIngredient,
-  } from "../reducers/cosmeticReducer";
-
-
-
+  initializeCosmetics,
+  //updateAllCosmeticIds,
+  //updateCurrentIndex,
+  //updateCurrentCosmetic,
+  nextCosmetic,
+  findCosmetic,
+  //clearSelectedIngredient,
+} from "../reducers/cosmeticReducer";
+import cosmetic from "../services/cosmetic";
 
 const Products = () => {
+  const [searchName, setSearchName] = useState("");
 
-    const [searchName, setSearchName] = useState("");
+  const dispatch = useDispatch();
+  const currentCosmetics = useSelector(
+    (state) => state.cosmetic.currentCosmetic
+  );
 
-    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(initializeCosmetics());
+  }, [dispatch]);
 
-    useEffect(() => {
-      dispatch(initializeCosmetics());
-    }, [dispatch]);
+  const all_cosmetics_cache = useSelector(
+    (state) => state.cosmetic.allCosmeticIds
+  );
 
+  const nextItem = async () => {
+    // dispatch(clearSelectedIngredient());
+    dispatch(nextCosmetic());
+  };
 
-    const all_cosmetics_cache = useSelector((state)=> state.cosmetic.allCosmeticIds);
-
-
-    const nextItem = async () => {
-      // dispatch(clearSelectedIngredient());
-        dispatch(nextCosmetic());
-      };
-
-
-      // handle searchbox text changes
+  // handle searchbox text changes
   const handleSearchBoxChange = async (e) => {
     setSearchName(e.target.value);
     e.preventDefault();
@@ -54,33 +53,34 @@ const Products = () => {
     }
   };
 
-return (
-    <div >
-        <div className="search-bar">
-            <label >
-                <input
-                    type="text"
-                    placeholder=" Search "
-                    className="search-field"
-                    value={searchName}
-                    onChange={(event) => handleSearchBoxChange(event)}
-                ></input>
-            </label>
-                <i className="gg-search"></i>
-        </div>
+  return (
+    <div>
+      <div className="search-bar">
+        <label>
+          <input
+            type="text"
+            placeholder=" Search "
+            className="search-field"
+            value={searchName}
+            onChange={(event) => handleSearchBoxChange(event)}
+          ></input>
+        </label>
+        <i className="gg-search"></i>
+      </div>
 
-        <Cosmetic />
-        <div className="button-wrapper">
-            <button className="button" onClick={nextItem}>
-                <span>
-                    Next Item
-                </span>
-            </button>
-        </div>
+      <div className="cosmetic-wrapper">
+        {currentCosmetics.map((cosmetic, index) => (
+          <Cosmetic key={index} cosmetic={cosmetic} />
+        ))}
+      </div>
+
+      <div className="button-wrapper">
+        <button className="button" onClick={nextItem}>
+          <span>More Item</span>
+        </button>
+      </div>
     </div>
-    );
-
-
+  );
 };
 
 export default Products;
