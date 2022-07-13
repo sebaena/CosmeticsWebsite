3ef424ce759f170cb408c6444527831ca4970d8e
yibzhang <zhangyiboturku@gmail.com>
2022-07-13 10:42:01 +0300

    update format, it seems like route has issue if there is space between path and element arguments

--- cosmeticapp/src/App.js
+++ cosmeticapp/src/App.js
@@ -1,14 +1,9 @@
 import Login from "./components/Login";
 import Products from "./components/Products";
 import Home from "./components/Home";
-import {
-  BrowserRouter as Router,
-  Routes, Route, Link
-} from "react-router-dom"
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-
   // const padding = {
   //   padding: 5
   // }
@@ -17,20 +12,27 @@ function App() {
     <Router>
       <nav className="navbar">
         <div className="navbar-menu">
-          <Link className="navbar-link" to = "/"> Home </Link>
-          <Link className="navbar-link" to = "/products"> Products </Link>
-          <Link className="navbar-link" to = "/signin"> Sign in </Link>
+          <Link className="navbar-link" to="/">
+            {" "}
+            Home{" "}
+          </Link>
+          <Link className="navbar-link" to="/products">
+            {" "}
+            Products{" "}
+          </Link>
+          <Link className="navbar-link" to="/signin">
+            {" "}
+            Sign in{" "}
+          </Link>
         </div>
       </nav>
       <Routes>
-        <Route path="/" element = {<Home />} />
-        <Route path="/products" element = {<Products />} />
-        <Route path="/signin" element = {<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/signin" element={<Login />} />
       </Routes>
     </Router>
-  )
-
-
+  );
 }
 
 export default App;
